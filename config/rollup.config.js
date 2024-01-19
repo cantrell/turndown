99d786b65ed128c513650a285a63c9d0e6da6c9c
Christian Cantrell <christian.cantrell@gmail.com>
2024-01-18 16:54:27 -0800

    Moved from domino to domino-ext

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,16 +1,19 @@
-import commonjs from '@rollup/plugin-commonjs'
-import replace from '@rollup/plugin-replace'
-import resolve from '@rollup/plugin-node-resolve'
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
 
 export default function (config) {
   return {
-    input: 'src/turndown.js',
+    input: "src/turndown.js",
     output: config.output,
-    external: ['domino'],
+    external: ["domino-ext"],
     plugins: [
       commonjs(),
-      replace({ 'process.browser': JSON.stringify(!!config.browser), preventAssignment: true }),
-      resolve()
-    ]
-  }
+      replace({
+        "process.browser": JSON.stringify(!!config.browser),
+        preventAssignment: true,
+      }),
+      resolve(),
+    ],
+  };
 }
